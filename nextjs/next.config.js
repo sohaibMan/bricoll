567d018f06381c5758a328a869a9001dfff2e1e3
Sohaib Manah <souhaibemanah@gmail.com>
2023-03-24 00:25:12 +0000

    config next js to accept pwa pulgin

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+const nextConfig = withPWA({
   reactStrictMode: true,
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
   },
-};
+});
 
 module.exports = nextConfig;
--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -2,12 +2,14 @@ import Layout from "../components/layout";
 
 export default function IndexPage() {
   return (
-    <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
-    </Layout>
+    <>
+      <Layout>
+        <h1>NextAuth.js Example</h1>
+        <p>
+          This is an example site to demonstrate how to use{" "}
+          <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
+        </p>
+      </Layout>
+    </>
   );
 }
