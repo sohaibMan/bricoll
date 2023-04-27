46a980da6d40ac37f80ae539d7954dd221861131
Sohaib Manah <souhaibemanah@gmail.com>
2023-04-27 19:03:37 +0100

    commit resolvers.ts file  because it needed in deployment

--- nextjs/pages/home.tsx
+++ nextjs/pages/home.tsx
@@ -1,37 +1,13 @@
-import { useEffect, useState } from "react";
-import io from "socket.io-client";
-let socket: any;
+import {useEffect, useState} from "react";
+
 
 const Home = () => {
-  const [input, setInput] = useState('')
+    const [input, setInput] = useState('')
 
-  useEffect(() => { socketInitializer() }, [])
 
-  const socketInitializer = async () => {
-    await fetch('/api/socket');
-    socket = io()
-
-    socket.on('connect', () => {
-      console.log('connected')
-    })
-
-    socket.on('update-input', (msg: any) => {
-      setInput(msg)
-    })
-  }
-
-  const onChangeHandler = (e: any) => {
-    setInput(e.target.value)
-    socket.emit('input-change', e.target.value)
-  }
-
-  return (
-    <input
-      placeholder="Type something"
-      value={input}
-      onChange={onChangeHandler}
-    />
-  )
+    return (
+        <div>HELLO </div>
+    )
 };
 
 export default Home;
