048d505d7340dce8d5695c01beabf3282aa5994b
Anas ZENAGUI <zenaguianas20@gmail.com>
2023-05-24 12:08:14 +0100

    Creating the first Step for Client user to make a registration + Fixing a bug

--- nextjs/components/auth/registration/firstStep.tsx
+++ nextjs/components/auth/registration/firstStep.tsx
@@ -1,111 +1,208 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import Button from "@mui/joy/Button";
-import {toast} from "react-hot-toast";
-import {multiStepContext} from "./stepContext";
-import {Divider, Stack} from "@mui/joy";
+import { toast } from "react-hot-toast";
+import { multiStepContext } from "./stepContext";
+import { Divider, Stack } from "@mui/joy";
 import Textarea from "@mui/joy/Textarea";
 import Input from "@mui/joy/Input";
-
+import { useSession } from "next-auth/react";
 
 export default function FirstStep() {
-    const {setStep, userData, setUserData} = useContext(multiStepContext);
+  const { setStep, userData, setUserData } = useContext(multiStepContext);
 
-    function handleSubmit() {
-        const requiredFields = [
-            "bio",
-            "country",
-            "city",
-            "phone",
-            "language",
-            "address",
-        ];
-        const missingFields = requiredFields.filter((field) => !userData[field]);
+  const { data: session } = useSession();
 
-        if (missingFields.length) {
-            toast.error(
-                `Please fill in the following fields: ${missingFields.join(", ")}`
-            );
-        } else {
-            setStep(2);
-        }
+  function handleSubmit() {
+    const freelancerRequiredFields = [
+      "bio",
+      "country",
+      "city",
+      "phone",
+      "language",
+      "address",
+    ];
+
+    const clientRequiredFields = [
+      "bio",
+      "address",
+      "country",
+      "city",
+      // "ZIP/Postal Code",
+      "postalCode",
+      "phone",
+      "language",
+      "timeZone",
+    ];
+
+    const requiredFields =
+      session?.user.userRole === "Freelancer"
+        ? freelancerRequiredFields
+        : clientRequiredFields;
+
+    const missingFields = requiredFields.filter((field) => !userData[field]);
+
+    if (missingFields.length) {
+      toast.error(
+        `Please fill in the following fields: ${missingFields.join(", ")}`
+      );
+    } else {
+      setStep(2);
     }
+  }
 
+  return (
+    <Stack spacing={2} sx={{ width: "50%", margin: "auto"}}>
+      {/*<Stack direction={"row"} spacing={2} sx={{paddingX: "10%"}}>*/}
 
-    return (
-        <Stack spacing={2} sx={{width: "50%", margin: "auto"}}>
-            {/*<Stack direction={"row"} spacing={2} sx={{paddingX: "10%"}}>*/}
-
-            <Stack direction={"row"} spacing={1}>
-
-                <Input
-                    sx={{width: "100%"}}
-                    placeholder="Phone"
-                    defaultValue={userData["phone"]}
-                    onChange={(e) => setUserData({...userData, phone: e.target.value})}
-                />
-                <Divider orientation="vertical"/>
-                <Input
-                    sx={{width: "100%"}}
-                    placeholder="Language"
-                    defaultValue={userData["language"]}
-                    onChange={(e) => {
-                        setUserData({...userData, language: e.target.value});
-                    }}
-                />
-            </Stack>
-            <Stack spacing={1} direction={"row"}>
-                <Input
-                    sx={{width: "100%"}}
-                    placeholder="City"
-                    defaultValue={userData["city"]}
-                    onChange={(e) => setUserData({...userData, city: e.target.value})}
-                />
-                {/*<Divider orientation="vertical"/>*/}
-                <Input
-                    sx={{width: "100%"}}
-                    placeholder="Country"
-                    defaultValue={userData["country"]}
-                    onChange={(e) =>
-                        setUserData({...userData, country: e.target.value})
-                    }
-                />
-
-                {/*<CountrySelector>*/}
-
-            </Stack>
-
+      {session?.user.userRole === "Freelancer" ? (
+        <>
+          <Stack direction={"row"} spacing={1}>
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Phone"
+              defaultValue={userData["phone"]}
+              onChange={(e) =>
+                setUserData({ ...userData, phone: e.target.value })
+              }
+            />
+            <Divider orientation="vertical" />
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Language"
+              defaultValue={userData["language"]}
+              onChange={(e) => {
+                setUserData({ ...userData, language: e.target.value });
+              }}
+            />
+          </Stack>
+          <Stack spacing={1} direction={"row"}>
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Country"
+              defaultValue={userData["country"]}
+              onChange={(e) =>
+                setUserData({ ...userData, country: e.target.value })
+              }
+            />
 
             <Input
-                sx={{width: "100%"}}
-                placeholder="Address"
-                defaultValue={userData["address"]}
-                onChange={(e) => {
-                    setUserData({...userData, address: e.target.value});
-                }}
+              sx={{ width: "100%" }}
+              placeholder="City"
+              defaultValue={userData["city"]}
+              onChange={(e) =>
+                setUserData({ ...userData, city: e.target.value })
+              }
             />
+            {/*<Divider orientation="vertical"/>*/}
 
+            {/*<CountrySelector>*/}
+          </Stack>
 
-            <Textarea
-                sx={{width: "100%"}}
-                minRows={4}
-                placeholder="bio"
-                defaultValue={userData["bio"]}
-                onChange={(e) => setUserData({...userData, bio: e.target.value})}
+          <Input
+            sx={{ width: "100%" }}
+            placeholder="Address"
+            defaultValue={userData["address"]}
+            onChange={(e) => {
+              setUserData({ ...userData, address: e.target.value });
+            }}
+          />
+
+          <Textarea
+            sx={{ width: "100%" }}
+            minRows={4}
+            placeholder="bio"
+            defaultValue={userData["bio"]}
+            onChange={(e) => setUserData({ ...userData, bio: e.target.value })}
+          />
+        </>
+      ) : (
+        <>
+          <Stack direction={"row"} spacing={1}>
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Phone"
+              defaultValue={userData["phone"]}
+              onChange={(e) =>
+                setUserData({ ...userData, phone: e.target.value })
+              }
             />
+            <Divider orientation="vertical" />
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Language"
+              defaultValue={userData["language"]}
+              onChange={(e) => {
+                setUserData({ ...userData, language: e.target.value });
+              }}
+            />
+          </Stack>
+          <Stack spacing={1} direction={"row"}>
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="Country"
+              defaultValue={userData["country"]}
+              onChange={(e) =>
+                setUserData({ ...userData, country: e.target.value })
+              }
+            />
+            <Input
+              sx={{ width: "100%" }}
+              placeholder="City"
+              defaultValue={userData["city"]}
+              onChange={(e) =>
+                setUserData({ ...userData, city: e.target.value })
+              }
+            />
+            {/*<Divider orientation="vertical"/>*/}
+            {/*<CountrySelector>*/}
+          </Stack>
 
+          <Input
+            sx={{ width: "100%" }}
+            placeholder="Address"
+            defaultValue={userData["address"]}
+            onChange={(e) => {
+              setUserData({ ...userData, address: e.target.value });
+            }}
+          />
 
-            {/*</Stack>*/}
-            <Button
-                // variant="contained"
-                color="primary"
-                type="submit"
-                onClick={handleSubmit}
-            >
+          <Input
+            sx={{ width: "100%" }}
+            placeholder="ZIP/Postal Code"
+            defaultValue={userData["postalCode"]}
+            onChange={(e) => {
+              setUserData({ ...userData, postalCode: e.target.value });
+            }}
+          />
 
+          <Input
+            sx={{ width: "100%" }}
+            placeholder="Time Zone"
+            defaultValue={userData["timeZone"]}
+            onChange={(e) => {
+              setUserData({ ...userData, timeZone: e.target.value });
+            }}
+          />
 
-                Next
-            </Button>
-        </Stack>
-    )
-        ;
+          <Textarea
+            sx={{ width: "100%" }}
+            minRows={4}
+            placeholder="Bio"
+            defaultValue={userData["bio"]}
+            onChange={(e) => setUserData({ ...userData, bio: e.target.value })}
+          />
+        </>
+      )}
+
+      {/*</Stack>*/}
+      <Button
+        // variant="contained"
+        color="primary"
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Next
+      </Button>
+    </Stack>
+  );
 }
