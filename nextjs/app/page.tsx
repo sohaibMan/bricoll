9043b614014e742c0a824387420712ff55f29e9c
Anas ZENAGUI <zenaguianas20@gmail.com>
2023-05-27 18:15:21 +0100

    Complete the landing page

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -170,7 +170,193 @@ const Home = () => {
           />
         </div>
       </section>
-      
+      <section className="my-12 bg-newColor" id="guids">
+        <div className="container mx-auto grid gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-2">
+          <div className="p-4">
+            <h2 className="text-5xl text-second mx-16 my-20 font-medium mb-2">
+              There are good talents here that will help you solve your problems{" "}
+            </h2>
+            <p className="text-second my-8 mx-16 text-xl font-normal">
+              There Are Many Talents That You Can Find. Of Course. In All Fields
+              In The World Of Work.
+            </p>
+          </div>
+          <div className="p-4 my-20">
+            <p className="bg-primary py-1 rounded-full w-5/6"></p>
+            <div className="container my-2 mx-auto grid gap-6 grid-cols-1 sm:grid-cols-1 md:grid-cols-2">
+              <div className="p-4 w-64">
+                <h2 className="text-5xl text-black my-2 font-semibold mb-2">
+                  4.9/5
+                </h2>
+                <p className="text-second my-8 text-xl font-normal">
+                  Average rating of talent from 2M+ reviews
+                </p>
+              </div>
+              <div className="p-4 w-64">
+                <h2 className="text-5xl text-black my-2 font-semibold mb-2">
+                  8K+
+                </h2>
+                <p className="text-second my-8 text-xl font-normal">
+                  Skills represented by talent on Borongan
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Image
+          className="py-5 mx-auto"
+          src={thumb}
+          alt="thumb"
+          width={428}
+          height={321}
+        />
+      </section>
+      <section className="my-12 bg-white" id="comments">
+        <h1 className="font-bold mx-auto text-4xl text-center md:text-5xl text-second">
+          Client’s Comments
+        </h1>
+        <p className="text-second my-8 mx-20 text-2xl font-normal">
+          At <span className="text-primary">BRICOL</span>, we pride ourselves on
+          providing a platform that connects freelancers with clients in a way
+          that is safe, secure, and reliable. But don't just take our word for
+          it - here's what some of our satisfied clients have to say
+        </p>
+        <div className="container mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2">
+          <div
+            className={`bg-white mx-16 my-16 rounded-lg md:mx-36 md:my-24 shadow-md transition-transform duration-300 transform hover:-translate-y-1 hover:shadow-xl ${
+              isHovered ? "scale-105" : ""
+            }`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div className="flex justify-center">
+              <Image
+                className="w-20 h-20 rounded-full object-cover"
+                src={men}
+                alt="Person 1"
+              />
+            </div>
+            <p className="text-center my-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              euismod massa ut magna efficitur, eget tempus nunc iaculis.
+              Aliquam erat volutpat. Sed rhoncus velit non felis maximus
+              sollicitudin."
+            </p>
+            <p className="text-center font-semibold">- Sohaib</p>
+          </div>
+          <div
+            className={`bg-white mx-16 my-16 rounded-lg md:mx-36 md:my-24 shadow-md transition-transform duration-300 transform hover:-translate-y-1 hover:shadow-xl ${
+              isHovered ? "scale-105" : ""
+            }`}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <div className="flex justify-center">
+              <Image
+                className="w-20 h-20 rounded-full object-cover"
+                src={women}
+                alt="Person 2"
+              />
+            </div>
+            <p className="text-center my-4">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+              euismod massa ut magna efficitur, eget tempus nunc iaculis.
+              Aliquam erat volutpat. Sed rhoncus velit non felis maximus
+              sollicitudin."
+            </p>
+            <p className="text-center font-semibold">- Sara</p>
+          </div>
+        </div>
+      </section>
+
+      {/* <section className="my-12 bg-white" id="comments">
+        <h1 className="font-bold mx-auto text-4xl text-center md:text-5xl text-second">
+          Client’s Comments
+        </h1>
+        <p className="text-second my-8 mx-20 text-2xl font-normal">
+          At <span className="text-primary">BRICOL</span>, we pride ourselves on
+          providing a platform that connects freelancers with clients in a way
+          that is safe, secure, and reliable. But don't just take our word for
+          it - here's what some of our satisfied clients have to say
+        </p>
+      </section> */}
+
+      <footer>
+        <footer className="bg-primary py-10">
+          <div className="container mx-auto text-center">
+            <h3 className="text-white text-xl font-bold mb-4">Contact Us</h3>
+            <p className="text-white text-lg mb-6">
+              We'd love to hear from you!
+            </p>
+            <div className="flex justify-center">
+              <a
+                href="tel:+1234567890"
+                className="mr-4 text-white hover:text-second"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 inline-block"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+                +212 72 567-8930
+              </a>
+              <a
+                href="mailto:info@bricol.com"
+                className="text-white hover:text-second"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 inline-block"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+                  />
+                </svg>
+                info@bricol.com
+              </a>
+            </div>
+            <div className="mt-8">
+              <ul className="flex justify-center space-x-6">
+                <li>
+                  <a href="#" className="text-white hover:text-second">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white hover:text-second">
+                    Services
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white hover:text-second">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white hover:text-second">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </footer>
     </>
   );
 };
