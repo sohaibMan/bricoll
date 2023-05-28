7e56eaf34dfda54143df12359727160120b305d0
Anas Zenagui <zenaguianas20@gmail.com>
2023-05-28 18:12:50 +0100

    Creating the UI for chatRoom

--- nextjs/pages/api/messages.ts
+++ nextjs/pages/api/messages.ts
@@ -1,6 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import {pusher} from '../../lib/pusher';
-import {ObjectId} from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { pusher } from "../../lib/pusher";
+import { ObjectId } from "mongodb";
+import db from "../../lib/mongodb";
 
 interface Message {
   id: ObjectId;
@@ -10,24 +11,34 @@ interface Message {
   createdAt: Date;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { senderUserId, receiverUserId, text } = req.body;
 
   if (!receiverUserId || !text) {
-    res.status(400).send('Bad request');
+    res.status(400).send("Bad request");
     return;
   }
 
-  const message: Message = {
-    id: new ObjectId(), // generate a random ID for the message
-    senderUserId,
-    receiverUserId,
+  const chat = await db.collection("chats").insertOne({
+    senderUserId: new ObjectId(senderUserId),
+    receiverUserId: new ObjectId(receiverUserId),
     text,
-    createdAt: new Date()
+    createdAt: new Date(),
+  });
+
+  const message: Message = {
+    id: chat.insertedId,
+    senderUserId: new ObjectId(senderUserId),
+    receiverUserId: new ObjectId(receiverUserId),
+    text,
+    createdAt: new Date(),
   };
   // console.log(`private-chat-1`)
 
- await pusher.trigger(`private-chat-1`, 'new-message', message);
+  await pusher.trigger(`private-chat-1`, "new-message", message);
 
   res.status(200).json(message);
 }
