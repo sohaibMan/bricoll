c9625b547ad5ded15e4d2da41d229edd6a547590
Anas Zenagui <zenaguianas20@gmail.com>
2023-06-04 18:07:27 +0100

    Fixing a bug

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,18 +1,26 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-    register: true,
-    skipWaiting: true,
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
 });
 const nextConfig = withPWA({
-    reactStrictMode: true,
-    webpack(config) {
-        config.experiments = {...config.experiments, topLevelAwait: true};
-        return config;
-    },
-    output: 'standalone',
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+        port: "",
+      },
+    ],
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
+  },
+  output: "standalone",
 });
 
 module.exports = nextConfig;
